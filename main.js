8f8a65d1781de754af878a40ce874ffc43c948fa
Tyler Brown <tylerbrown487@gmail.com>
2023-07-16 12:33:02 -0400

    added sections for each player to index.html and deleted code from main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-player API = "http://localhost:3000/players";
+const playerAPI = "http://localhost:3000/players";
 
 let addPlayer = false;
 
@@ -16,62 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-fetch("http://localhost:3000/players")
-  .then((resp) => resp.json())
-  .then((players) => player.forEach((player) => renderPlayer(player)));
+//fetch the players
 
-const playerCollection = document.querySelector("#player-collection");
+fetch(playerAPI)
+  .then((res) => res.json())
+  .then(console.log);
 
-function renderPlayer(player) {
-  const playerCard = document.createElement("div");
-  playerCard.className = "card";
-
-  const h2 = document.createElement("h2");
-  h2.textContent = player.name;
-
-  const img = document.createElement("img");
-  img.src = player.image;
-  img.className = "player-avatar";
-
-  const p = document.createElement("p");
-  p.textContent = `${player.likes} likes`;
-
-  const button = document.createElement("button");
-  button.className = "like-btn";
-  button.id = player.id;
-  button.textContent = "Like";
-
-  playerCard.append(h2);
-  playerCard.append(img);
-  playerCard.append(p);
-  playerCard.append(button);
-
-  playerCollection.appendChild(playerCard);
-
-  button.addEventListener("click", increaseLikes);
-
-  function increaseLikes() {
-    player.likes = player.likes + 1;
-    p.textContent = `${player.likes} likes`;
-  }
-}
-
-const playerForm = document.querySelector(".add-player-form");
-
-function handleSubmit(e) {
-  e.preventDefault();
-
-  const newPlayerName = e.target.name.value;
-  const newPlayerImage = e.target.image.value;
-
-  const newPlayer = {
-    id: 10,
-    name: newPlayerName,
-    image: newPlayerImage,
-    likes: 0,
-  };
-
-  renderPlayer(newPlayer);
-}
-
-playerForm.addEventListener("submit", handleSubmit);
+//const playerCollection = document.querySelector("#player-collection");

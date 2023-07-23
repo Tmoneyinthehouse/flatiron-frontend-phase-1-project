f0d85db8d9bcbb2401c57245ce98e93529f0db5d
Tyler Brown <tylerbrown487@gmail.com>
2023-07-23 11:15:20 -0400

    final edits to project

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("main.js connected");
+console.log("connected");
 
 const playerAPI = "http://localhost:3000/players";
 
@@ -7,8 +7,8 @@ let addPlayer = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-player-btn");
   const PlayerFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addPlayer = !addPlayer;
     if (addPlayer) {
       PlayerFormContainer.style.display = "block";
@@ -16,58 +16,71 @@ document.addEventListener("DOMContentLoaded", () => {
       PlayerFormContainer.style.display = "none";
     }
   });
+
+  fetch(playerAPI)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((players) => players.forEach((player) => renderPlayers(player)))
+    .catch((error) => console.error("Error fetching data:", error));
+
+  const playerCollection = document.querySelector("#player-collection");
+
+  function renderPlayers(player) {
+    const playerCard = document.createElement("div");
+    playerCard.className = "card";
+
+    const h2 = document.createElement("h2");
+    h2.textContent = player.name;
+
+    const img = document.createElement("img");
+    img.src = player.image;
+    img.className = "player-avatar";
+
+    const p = document.createElement("p");
+    p.textContent = `${player.likes} likes`;
+
+    const button = document.createElement("button");
+    button.className = "like-btn";
+    button.id = player.id;
+    button.textContent = "Like";
+
+    playerCard.append(h2);
+    playerCard.append(img);
+    playerCard.append(p);
+    playerCard.append(button);
+
+    playerCollection.appendChild(playerCard);
+
+    button.addEventListener("click", increaseLikes);
+
+    function increaseLikes() {
+      player.likes = player.likes + 1;
+      p.textContent = `${player.likes} likes`;
+    }
+  }
+
+  const playerForm = document.querySelector(".add-player-form");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newPlayerName = e.target.name.value;
+    const newPlayerImage = e.target.image.value;
+
+    const newPlayer = {
+      id: 0,
+      name: newPlayerName,
+      image: newPlayerImage,
+      likes: 0,
+    };
+
+    renderPlayers(newPlayer);
+    e.target.reset();
+  }
+
+  playerForm.addEventListener("submit", handleSubmit);
 });
-
-//fetch the players
-
-fetch(playerAPI)
-  .then((res) => res.json())
-  .then(console.log);
-
-//const playerCollection = document.querySelector("#player-collection");
-
-// const cardData = [
-  {
-    heading: 'card 1',
-    body: 'this is card body 1'
-  }
-  {
-    heading: 'card 2',
-    body: 'this is card body 2'
-  }
-  {
-    heading: 'card 3',
-    body: 'this is card body 3'
-  }
-  {
-    heading: 'card 4',
-    body: 'this is card body 4'
-  }
-  {
-    heading: 'card 5',
-    body: 'this is card body 5'
-  }
-  {
-    heading: 'card 6',
-    body: 'this is card body 6'
-  }
-  {
-    heading: 'card 7',
-    body: 'this is card body 7'
-  }
-  {
-    heading: 'card 8',
-    body: 'this is card body 8'
-  }
-  {
-    heading: 'card 9',
-    body: 'this is card body 9'
-  }
-  {
-    heading: 'card 10',
-    body: 'this is card body 10'
-  }
-
-]
-  
-
